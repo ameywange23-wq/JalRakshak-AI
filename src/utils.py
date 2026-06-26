cece4c8745b00a3b2ad46c46b57f1392fae3a2b9
ameywange23-wq <amey.wange23@mmit.edu.in>
2026-06-26 14:26:05 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,10 @@
+import logging
 
+logging.basicConfig(
+    filename="logs/preprocessing.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s"
+)
+
+def log_message(message):
+    logging.info(message)
