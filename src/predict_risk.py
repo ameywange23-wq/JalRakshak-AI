b4a4f73b153ab3852bd3f0c2f4277286b658fa51
ameywange23-wq <amey.wange23@mmit.edu.in>
2026-06-26 14:06:55 +0530

    Update predict_risk.py

--- src/predict_risk.py
+++ src/predict_risk.py
@@ -1,1 +1,18 @@
+def predict_risk(ph, tds, turbidity, do):
 
+    if (
+        tds > 700 or
+        turbidity > 8 or
+        ph < 6 or
+        ph > 8.5
+    ):
+        return "High Risk"
+
+    elif (
+        tds > 400 or
+        turbidity > 4
+    ):
+        return "Moderate Risk"
+
+    else:
+        return "Safe"
