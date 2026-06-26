ee3e665ab7ec81764604e93b80ea4303231ef545
ameywange23-wq <amey.wange23@mmit.edu.in>
2026-06-26 14:08:58 +0530

    Update multilingual_alerts.py

--- src/src/multilingual_alerts.py
+++ src/src/multilingual_alerts.py
@@ -1,1 +1,23 @@
+def generate_alert(risk):
 
+    if risk == "High Risk":
+
+        return {
+            "English": "Water quality is unsafe.",
+            "Hindi": "पानी की गुणवत्ता असुरक्षित है।",
+            "Marathi": "पाण्याची गुणवत्ता असुरक्षित आहे."
+        }
+
+    elif risk == "Moderate Risk":
+
+        return {
+            "English": "Water quality needs monitoring.",
+            "Hindi": "पानी की गुणवत्ता पर निगरानी रखें।",
+            "Marathi": "पाण्याच्या गुणवत्तेवर लक्ष ठेवा."
+        }
+
+    return {
+        "English": "Water quality is safe.",
+        "Hindi": "पानी की गुणवत्ता सुरक्षित है।",
+        "Marathi": "पाण्याची गुणवत्ता सुरक्षित आहे."
+    }
