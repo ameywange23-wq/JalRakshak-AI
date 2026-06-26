742795c2fba090105e2ed3990b9812f185c404c1
ameywange23-wq <amey.wange23@mmit.edu.in>
2026-06-26 14:17:07 +0530

    Update forecast.py

--- dashboard/src/forecast.py
+++ dashboard/src/forecast.py
@@ -1,1 +1,25 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
+def forecast_tds():
+
+    days = [1, 2, 3, 4, 5]
+
+    tds = [300, 320, 350, 370, 400]
+
+    model = LinearRegression()
+
+    X = pd.DataFrame(days)
+
+    model.fit(X, tds)
+
+    future_day = pd.DataFrame([6])
+
+    prediction = model.predict(
+        future_day
+    )
+
+    return round(
+        prediction[0],
+        2
+    )
